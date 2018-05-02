070499394a16fab345ade3b64f3b463de0f51072
柳天 <liutian01@fenrir-tec.com>
2018-05-02 17:33:15 +0800

    18.5.2

--- demo/demo/ViewController.swift
+++ demo/demo/ViewController.swift
@@ -8,9 +8,15 @@
 
 import UIKit
 
-var courses = [["title":"a","btntitle":"btntitle"],["title":"","btntitle":"btntitle"],["title":"a","btntitle":"btntitle"],["title":"a","btntitle":"btntitle"],["title":"a","btntitle":"btntitle"],["title":"a","btntitle":"btntitle"]]
+var courses = [["title":"《我们》","btntitle":"btntitle"],
+               ["title":"陈奕迅","btntitle":"btntitle"],
+               ["title":"该说的 别说了","btntitle":"btntitle"],
+               ["title":"你懂得 就够了","btntitle":"btntitle"],
+               ["title":"真的有 某一种悲哀","btntitle":"btntitle"],
+               ["title":"连泪也不能流","btntitle":"btntitle"],
+               ["title":"只能目送","btntitle":"btntitle"]]
 
-
+var selectedCellIndexPath:IndexPath!
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
@@ -28,17 +34,40 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return courses.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:TableViewCell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell") as! TableViewCell
+        cell.cellTitle.translatesAutoresizingMaskIntoConstraints = false
+        
+        let textview = UITextView(frame: CGRect.zero)
+        textview.translatesAutoresizingMaskIntoConstraints = false
+        textview.textColor = UIColor.red
+        textview.text = "创建各单元显示内容(创建参数indexPath指定的单元,UIDatePicker是一个控制器类,封装了UIPickerView,但是他是UIControl的子类"
         let item = courses[indexPath.row]
+        cell.contentView.addSubview(textview)
         cell.cellTitle.text = item["title"]
-        cell.cellButton.label = item["btntitle"]
+        cell.cellButton.setTitle(item["btntitle"], for: .normal)
         return cell
     }
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.tableView.deselectRow(at: indexPath, animated: false)
+        selectedCellIndexPath = indexPath
+        tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if(selectedCellIndexPath != nil && selectedCellIndexPath == indexPath){
+            return 140
+        }
+        return 60
+    }
+
 }
 
