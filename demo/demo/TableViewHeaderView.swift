1e6f129a8f5d851828486e6f569ec9602aae529b
柳天 <liutian01@fenrir-tec.com>
2018-05-10 09:53:28 +0800

    5.10
    用insert/delete rows 的方式实现tableview 展开收缩

--- demo/demo/TableViewHeaderView.swift
+++ demo/demo/TableViewHeaderView.swift
@@ -12,6 +12,7 @@ protocol TableViewHeaderDelegate {
     func toggleSection(_ header: TableViewHeaderView, section: Int)
 }
 
+
 class TableViewHeaderView: UITableViewHeaderFooterView {
     
     var delegate: TableViewHeaderDelegate?
@@ -51,20 +52,18 @@ class TableViewHeaderView: UITableViewHeaderFooterView {
             //
             headerlabel.rotate(collapsed ? 0.0 : .pi / 2)
         }
-    
 }
 
-
-extension UIView {
-
-    func rotate(_ toValue: CGFloat, duration: CFTimeInterval = 0.2) {
-        let animation = CABasicAnimation(keyPath: "transform.rotation")
-
-        animation.toValue = toValue
-        animation.duration = duration
-        animation.isRemovedOnCompletion = false
-        animation.fillMode = kCAFillModeForwards
-
-        self.layer.add(animation, forKey: nil)
-}
-}
+//extension UIView {
+//
+//    func rotate(_ toValue: CGFloat, duration: CFTimeInterval = 0.2) {
+//        let animation = CABasicAnimation(keyPath: "transform.rotation")
+//
+//        animation.toValue = toValue
+//        animation.duration = duration
+//        animation.isRemovedOnCompletion = false
+//        animation.fillMode = kCAFillModeForwards
+//
+//        self.layer.add(animation, forKey: nil)
+//}
+//}
